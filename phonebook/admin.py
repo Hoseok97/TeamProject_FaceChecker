92cb67474de112208625c430a14fc9ed15c9c283
TripleLux <98064761+TripleLux@users.noreply.github.com>
2022-06-13 09:46:22 +0900

    Delete phonebook directory

--- phonebook/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from phonebook.models import PhoneBook
-
-# Register your models here.
-class PhoneBookAdmin(admin.ModelAdmin):
-    list_display = ('name','telnum','email','birth')
-
-admin.site.register(PhoneBook, PhoneBookAdmin)
