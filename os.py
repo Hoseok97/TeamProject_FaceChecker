07953bdc2d2bea1dd24dc8016e631b0503d1f9ac
TripleLux <98064761+TripleLux@users.noreply.github.com>
2022-06-13 09:45:57 +0900

    Delete os.py

--- os.py
+++
@@ -1,5 +0,0 @@
-import os
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
-
-#python train.py --img 96 --batch 32 --epochs 5 --data data/coco128.yaml --cfg models/yolov5s.yaml --weights  weights/yolov5s.pt
